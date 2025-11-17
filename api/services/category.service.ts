c95acdf6267988a3106b9652152f4e182a52574d
diego brito <diegobritosilva17@gmail.com>
2025-11-17 20:00:06 -0300

    feat: Add authorization headers to category service API calls

--- api/services/category.service.ts
+++ api/services/category.service.ts
@@ -1,5 +1,6 @@
 import axios, { type AxiosInstance } from "axios"
 import type { CategoryRequestDto } from "../types/category.type"
+import { getAuthToken } from "../utils/auth"
 
 class CategoryService {
   private api: AxiosInstance
@@ -8,23 +9,36 @@ class CategoryService {
     this.api = axios.create({ baseURL })
   }
 
+  private getHeaders() {
+        const token = getAuthToken()    
+        return token ? { Authorization: `Bearer ${token}` } : {}
+  }
+
   async getCategories(): Promise<CategoryRequestDto[]> {
-    const response = await this.api.get<CategoryRequestDto[]>("/categories")
+    const response = await this.api.get<CategoryRequestDto[]>("/categories", {
+      headers: this.getHeaders()
+    })
     return response.data
   }
 
   async getCategoryById(id: number): Promise<CategoryRequestDto> {
-    const response = await this.api.get<CategoryRequestDto>(`/categories/${id}`)
+    const response = await this.api.get<CategoryRequestDto>(`/categories/${id}`, {
+      headers: this.getHeaders()
+    })
     return response.data
   }
 
   async createCategory(data: CategoryRequestDto): Promise<CategoryRequestDto> {
-    const response = await this.api.post<CategoryRequestDto>("/categories", data)
+    const response = await this.api.post<CategoryRequestDto>("/categories", data, {
+        headers: this.getHeaders()
+    })
     return response.data
   }
 
   async updateCategory(id: number, data: CategoryRequestDto): Promise<CategoryRequestDto> {
-    const response = await this.api.put<CategoryRequestDto>(`/categories/${id}`, data)
+    const response = await this.api.put<CategoryRequestDto>(`/categories/${id}`, data, {
+        headers: this.getHeaders()
+    })
     return response.data
   }
 
